4797cba1fad98087b57d4ea05e5ea9144fc65a42
Dias <dias.zhumabekov790@gmail.com>
2023-11-13 16:00:32 +0600

    test

--- src/main/java/kz/projectx/almatour/config/SecurityConfig.java
+++ src/main/java/kz/projectx/almatour/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
     private final AuthenticationService userService;
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
